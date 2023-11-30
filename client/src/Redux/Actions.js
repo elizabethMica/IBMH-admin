4c5b9a2190b4691368ba2e6fa23398a8c2f2f80c
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-30 14:57:22 -0300

    reducer switch

---
+++ client/src/Redux/Actions.js
@@ -0,0 +1,115 @@
+import { POST_SERMON, UPDATE_SERMON, DELETE_SERMON, DELETE_CONTACT, GET_CONTACT, GET_SERMONS, GET_SERMON_DETAIL } from "./Action-types";
+
+export function getAllSermon (){
+    return async function(dispatch){
+        try {
+            const response = (await axios.get('http://localhost:3001/sermon')).data
+            dispatch({
+                type: GET_SERMONS,
+                payload: response
+            })
+        } catch (error) {
+            throw Error(error.message)
+        }
+    }
+};
+
+export function postSermon (values){
+    return async function(dispatch){
+        try {
+            const headers = {'Content-Type':'application/json'}
+            const response = (await axios.post("http://localhost:3001/sermon-post", values, {headers})).data
+            dispatch({
+               type: POST_SERMON,
+               payload: response
+            })
+        } catch (error) {
+            throw Error(error.message)
+        }
+    }
+};
+
+export function updateSermon (values){
+    return async function(dispatch){
+        try {
+            const headers = {'Content-Type':'application/json'}
+            const response = (await axios.patch(`http://localhost:3001/sermon-update/${id}` , values, {headers})).data
+            dispatch({
+                type: UPDATE_SERMON,
+                payload: response
+            })
+        } catch (error) {
+            throw Error(error.message)
+        }
+    }
+};
+
+export function deleteSermon (id){
+    return async function(dispatch){
+        try {
+            const response = (await axios.delete(`http://localhost:3001/sermon-delete/${id}`)).data
+            dispatch({
+                type: DELETE_SERMON,
+                payload: response
+            })
+        } catch (error) {
+            throw Error(error.message)
+        }
+    }
+};
+
+export function deleteContact (id){
+    return async function(dispatch){
+     try {
+         const response = (await axios.delete(`http://localhost:3001/contact-delete/${id}`)).data
+         dispatch({
+             type: DELETE_CONTACT,
+             payload: response
+         })
+     } catch (error) {
+         throw Error(error.message)
+     }
+    }
+ };
+
+ export function getAllContact (){
+    return async function(dispatch){
+      try {
+        const response = (await axios.get("http://localhost:3001/contact")).data
+        dispatch({
+            type: GET_CONTACT,
+            payload: response
+        })
+      } catch (error) {
+        throw Error(error.message)
+      }
+    }
+};
+
+export function getSermonDetail (id){
+    return async function(dispatch){
+        try {
+            const response = (await axios.get(`http://localhost:3001/sermon/${id}`)).data
+            dispatch({
+                type: GET_SERMON_DETAIL,
+                payload: response
+            })
+        } catch (error) {
+            throw Error(error.message)
+        }
+    }
+};
+
+// export function getContactDetail (id){
+//     return async function(dispatch){
+//         try {
+//             const response = (await axios.get(`http://localhost:3001/sermon/${id}`)).data
+//             dispatch({
+//                 type: GET_SERMON_DETAIL,
+//                 payload: response
+//             })
+//         } catch (error) {
+//             throw Error(error.message)
+//         }
+//     }
+// };
--- client/src/Redux/Reducer.js
+++ client/src/Redux/Reducer.js
@@ -1,13 +1,42 @@
+import {
+    GET_CONTACT,
+    GET_CONTACT_DETAIL,
+    GET_SERMONS,
+    GET_SERMON_DETAIL,
+    DELETE_CONTACT,
+    DELETE_SERMON,
+    UPDATE_SERMON,
+    POST_SERMON,
+} from './Action-types'
 
 let initialState ={
     sermons: [],
-
+    sermonDetail: [],
+    contacts: [],
+    
 }
 
 function rootReducer(state = initialState, {type, payload}){
     switch (type) {
-        case value:
-            
+        case POST_SERMON:
+            return{
+                ...state
+            }
+        case GET_SERMONS:
+            return{
+                ...state,
+                sermons: payload.toReversed()
+            }
+        case GET_CONTACT:
+            return{
+                ...state,
+                constacts: payload
+            }
+        case GET_SERMON_DETAIL:
+            return{
+                ...state,
+                sermonDetail: payload
+            }            
     
         default:
             return{
