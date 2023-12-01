61fd3306705137ef9ad2f56ff686b2e3c0e8f7e4
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-01 18:24:09 -0300

    post sermon done

--- client/src/Components/PostSermon/PostSermon.jsx
+++ client/src/Components/PostSermon/PostSermon.jsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import { getAllSermon, postSermon } from '../../Redux/Actions'
 import {books} from './arrayBooks'
+import { GoDotFill } from "react-icons/go";
+import { Link } from 'react-router-dom';
 
 function PostSermon() {
 
@@ -19,6 +21,9 @@ function PostSermon() {
         keywords: "",
         spotifyLink: ""
     })
+
+    const embed = "https://www.youtube.com/embed/"
+    const videoLink = value?.videoYT?.split("/")[3]
 
     const handleChange =(event)=>{
         event.preventDefault();
@@ -38,49 +43,59 @@ function PostSermon() {
     }
 
   return (
-    <div>
-        <h1>Subir sermón</h1>
+    <div className='flex flex-col md:flex-row py-24 md:gap-2'>
+        <div className='md:w-1/2 flex flex-col justify-center md:justify-start items-center'>
+        <h1 className='text-lg font-semibold underline decoration-gray-500'>Actualizar sermón</h1>
 
-        <form onSubmit={(event)=>handleSubmit(event)}>
-            <div>
-                <label> Titulo </label>
+        <form onSubmit={(event)=>handleSubmit(event)} className='w-[300px] md:w-full md:px-6 flex flex-col'>
+
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Titulo</label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Titulo de la predica"
                 name="title"
                 onChange={handleChange}/>
             </div>
 
-            <div>
-                <label> Imagen </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Imagen de portada (Link)</label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="URL de la imagen para la portada"
                 name="cover"
                 onChange={handleChange}/>
+
             </div>
 
-            <div>
-                <label> Fecha </label>
+            
+
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Fecha </label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Fecha de la predicación"
                 name="date"
                 onChange={handleChange}/>
             </div>
 
-            <div>
-                <label> Video </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Video </label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="URL del video de YouTube"
                 name="videoYT"
                 onChange={handleChange}/>
             </div>
 
-            <div>
-                <label> Cita </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Cita </label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Ej: Romanos 5:8"
                 name="verse"
@@ -88,39 +103,45 @@ function PostSermon() {
             </div>
 
            
-            <div>
-                <label> Libro </label>
-                <select onChange={(event)=> handleChange(event)} name="book">
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Libro </label>
+                <select onChange={(event)=> handleChange(event)} name="book" className='bg-gray-300 rounded-md'>
                     {
                         books?.map(b =>{
                             return <option value={b?.name}>{b?.name}</option>
                         })
                     }
                 </select>
-                <p>{value?.book}</p>
+                <div className='flex justify-start items-center my-2'>
+                <GoDotFill size={10}/>
+                <p className=''>{value?.book}</p>
+                </div>
             </div>
 
-            <div>
-                <label> Predicador </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Predicador </label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Nombre y Apellido"
                 name="preacher"
                 onChange={handleChange}/>
             </div>
 
-            <div>
-                <label> Versiculo </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Versiculo </label>
                 <textarea
+                className='w-full min-h-[150px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Texto del versiculo (sin números)"
                 name="verseText"
                 onChange={handleChange}/>
             </div>
 
-            <div>
-                <label> Descripción (Opcional) </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col mb-4'>
+                <label className='text-sm md:text-lg text-gray-500'> Descripción (Opcional) </label>
                 <textarea 
+                className='w-full min-h-[150px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Descripción (No versiculo)"
                 name="description"
@@ -137,17 +158,71 @@ function PostSermon() {
                 onChange={handleChange}/>
             </div> */}
 
-            <div>
-                <label> Audio de Spotify (Opcional) </label>
+            <div className='w-[300px] md:w-full md:px-6 flex flex-col'>
+                <label className='text-sm md:text-lg text-gray-500'> Audio de Spotify (Opcional) </label>
                 <input 
+                className='w-full h-[45px] border-2  rounded-md text-start'
                 type="text" 
                 placeholder="Link del audio de Spotify"
                 name="spotifyLink"
                 onChange={handleChange}/>
             </div>
 
-            <div><button type="submit" >Subir</button></div>
+            <div className='flex justify-center items-center '>
+                <button type="submit" className='mt-4 bg-green-500 px-2 rounded-md' >Actualizar</button>
+            </div>
         </form>
+        </div>
+
+
+        <div className='md:w-1/2 flex flex-col justify-start items-center pt-8 md:pt-0'>
+
+            {
+                value.cover ? (
+                  <div className=' flex flex-col justify-start items-center mb-8'>
+                     <h3 className='text-lg font-semibold underline decoration-gray-500 mb-4'>Portada</h3>
+                     <img src={value.cover} className='rounded-lg w-[300px] h-[200px] md:w-[480px] md:h-[300px]'/>
+                  </div>
+                ) : null
+            }
+            
+
+            <h3 className='text-lg font-semibold underline decoration-gray-500'>Preview</h3>
+            <h3 className="text-lg font-bold text-center md:text-3xl">{value.title}</h3>
+            {
+                value.verse ? (<div>
+                  <h5 className="text-sm md:text-lg text-center mb-2 w-full text-gray-500">{value.verse} | {value.preacher}</h5>
+                  </div>
+                ): null
+            }
+           
+
+        <section className="mx-8">
+            <div >
+                {value.videoYT? (
+                        <iframe
+                        key={value.verse}
+                        src={embed + videoLink }
+                        className="w-80 h-48 m-auto md:w-9/12 md:h-[260px]"
+                    />    
+                    ) : null
+                }
+            </div>
+            
+            <div className="mb-10 mt-4 border-t-2 md:mx-28 md:min-w-[300px] flex flex-col justify-center items-center">
+                <p className="mt-2 font-semibold text-sm md:text-lg ">Versículo</p>
+                <p className="mt-2 text-justify md:text-start md:text-xl">{value.verseText}</p>
+            </div>
+               
+            {
+            value.description ? ( <div className="mb-10 mt-4 border-t-2 md:mx-28">
+            <p className="mt-2 font-semibold text-sm md:text-lg ">Descripción</p>
+            <p className="mt-2 text-justify md:text-start md:text-xl">{value.description}</p>
+            </div>) : null
+            }
+        </section>
+        
+        </div>
     </div>
   )
 }
