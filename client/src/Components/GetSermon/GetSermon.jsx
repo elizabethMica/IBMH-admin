5c2b736400925b8fe1ca3c5f4ad0b6e85bf84e0e
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-01 14:35:28 -0300

    sermon home

--- client/src/Components/GetSermon/GetSermon.jsx
+++ client/src/Components/GetSermon/GetSermon.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import {  useDispatch, useSelector } from 'react-redux'
 import { getAllSermon } from '../../Redux/Actions'
+import SermonCard from './SermonCard'
 
 function GetSermon() {
 
@@ -11,11 +12,30 @@ function GetSermon() {
      },[dispatch])
 
   return (
-    <div>
-        <h1>Sermones</h1>
+    <div className='pt-24 flex flex-col justify-center items-center'>
+        <h1 className='text-2xl md:text-3xl font-semibold border-b-2 '>Sermones</h1>
+        <div className='mx-8 pt-4 gap-4 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-start'>
         {sermones?.map(s => {
-            return <div>{s.title}</div>
+             return(
+              <div className=' flex justify-center items-center md:my-4'>
+                <SermonCard
+                 key={s.id}
+                 id={s.id}
+                 title={s.title}
+                 verse={s.verse}
+                 verseText={s.verseText}
+                 preacher={s.preacher}
+                 book={s.book}
+                 keywords={s.keywords}
+                 description={s.description}
+                 cover={s.cover}
+                 videoYT={s.videoYT}
+                 date={s.date} 
+                />
+                </div>
+            )
         })}
+        </div>
     </div>
   )
 }
