36e31bba47e3cdc4cc9e7b71998f3ceb2d992393
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-02 21:19:12 -0300

    contact card style

--- client/src/Components/GetContact/ContactCard.jsx
+++ client/src/Components/GetContact/ContactCard.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 function ContactCard(Props) {
 
   return (
     <div className='flex flex-col md:flex-row justify-center items-start md:items-center mx-8 my-4 border-2  rounded-md p-2'>
-      <p>{Props.name}</p>
-      <p>{Props.lastName}</p>
-      <p>{Props.phone}</p>
-      <p>{Props.email}</p>
+        <div className='flex gap-2 border-b-2 w-full'>
+            <p>{Props.name}</p>
+            <p>{Props.lastName}</p>
+        </div>
+        <div className='flex flex-col text-sm justify-start items-start w-full rounded-md'>
+            <div className='flex gap-2'>
+                <FaPhone/>
+                <p> {Props.phone} </p>
+            </div>
+            <div className='flex gap-2'>
+                <MdEmail/>
+                <p> {Props.email} </p>
+            </div>
+            
+        </div>
       <p>{Props.message}</p>
     </div>
   )
