b151a58787a3e6c9e87cdc8a40f4da91c384a0e2
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-02 21:05:15 -0300

    landing padding

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 function Landing() {
   return (
-    <div className='md:pt-14 pt-24 flex justify-center items-center flex-col '>
+    <div className='md:pt-14 pt-24 pb-14 flex justify-center items-center flex-col '>
         <img src={logo} alt="" className='w-1/2 md:w-[450px] h-full'/>
         <div className='flex flex-col justify-center items-center md:flex-row gap-2 flex-wrap mt-4'>
         <NavLink to="/sermones"><button className='bg-[#be8552] my-4 rounded-lg text-lg font-semibold px-2 py-1'>Sermones</button></NavLink>
