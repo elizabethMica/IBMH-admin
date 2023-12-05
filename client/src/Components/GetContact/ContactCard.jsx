5624bd0f924a165c079c261e1849d7cc4fd5f240
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-05 19:02:21 -0300

    landing cards

--- client/src/Components/GetContact/ContactCard.jsx
+++ client/src/Components/GetContact/ContactCard.jsx
@@ -4,11 +4,7 @@ import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 function ContactCard(Props) {
-
-  
-
   return (
-       
         <div className='flex flex-col md:flex-row md:gap-4 justify-center items-center  mx-8 my-4 border-2  rounded-md p-2'>
         <div className='flex gap-2 justify-center items-center border-b-2 md:border-r-2 md:border-b-0 md:justify-start md:mx-2 w-full'>
             <p className='text-lg md:text-2xl font-semibold text-gray-500'>{Props.name}</p>
@@ -28,7 +24,7 @@ function ContactCard(Props) {
             <p>{Props.message}</p>
         </div> */}
         <div className='flex md:w-[400px] justify-end md:justify-center items-center md:mr-2  '>
-        <NavLink to={`/contact/${Props.id}`}><button className='bg-gray-300 px-2 py-1 rounded-md hover:bg-gray-400'>Ver detalle</button></NavLink>
+        <NavLink to={`/contact/${Props.id}`}><button className='bg-gray-300 px-2 py-1 rounded-md hover:bg-gray-400'>Administrar</button></NavLink>
         </div>
     </div>
     
--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -2,19 +2,24 @@ import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import logo from '/logoBlack.png'
 import { NavLink } from 'react-router-dom'
-import {getAllSermon} from '../../Redux/Actions'
+import {getAllContact, getAllSermon} from '../../Redux/Actions'
 import SermonCard from '../GetSermon/SermonCard'
 import { FaPlus} from "react-icons/fa6";
+import ContactCard from '../GetContact/ContactCard'
 
 function Landing() {
 
   const dispatch = useDispatch()
+
   useEffect(()=>{
    window.scrollTo(0,0)
    dispatch(getAllSermon())
+   dispatch(getAllContact())
   },[])
 
   const sermons = useSelector(state => state.sermons)
+  const contacts = useSelector(state => state.contacts)
+  const lastFour = contacts.slice(-4)
   const lastThree = sermons.slice(-3)
   return (
     // <div className='md:pt-14 pt-24 pb-14 flex justify-center items-center flex-col '>
@@ -26,26 +31,50 @@ function Landing() {
     //     </div>
        
     // </div>
-    <div className='py-24 md:mx-14'>
-      <div className='flex justify-between items-center mb-4'>
-      <h3 className='text-xl font-semibold '>Sermones</h3>
-      <NavLink><button className=' rounded-lg bg-[#be8552] border-2 border-[#be8552] px-4 my-4'>Ver más sermones</button></NavLink>
-      </div>
-      <div  className='flex flex-col justify-center items-center md:items-start md:flex-row md:flex-wrap md:gap-10'>
-        <NavLink to="/post-sermon"> <div  className='flex justify-center items-center bg-gray-100 text-center my-6 md:my-0 w-72 md:w-64 md:h-64 h-72 border-dashed border-2 border-gray-400 rounded-lg'><button className='text-center'><FaPlus size={30}className='text-gray-400'/></button></div></NavLink>
-        {lastThree?.map(s =>{
-          return(
-            <SermonCard
-            key={s.id}
-            title={s.title}
-            date={s.date}
-            verse={s.verse}
-            description={s.description}
-            cover={s.cover}
-            />
-          )
-        })}
-      </div>
+    <div className='pt-24 md:mx-14 mx-8 pb-16'>
+        <div className='flex justify-between items-center mb-4 '>
+          <h3 className='text-xl font-semibold '>Sermones</h3>
+          <NavLink><button className=' rounded-lg bg-[#be8552] border-2 border-[#be8552] px-4 my-4'>Ver más sermones</button></NavLink>
+        </div>
+        <div  className='flex flex-col justify-center items-center md:items-start md:flex-row md:flex-wrap md:gap-10'>
+          <NavLink to="/post-sermon"> <div  className='flex justify-center items-center bg-gray-100 text-center my-6 md:my-0 w-72 md:w-64 md:h-64 h-72 border-dashed border-2 border-gray-400 rounded-lg'><button className='text-center'><FaPlus size={30}className='text-gray-400'/></button></div></NavLink>
+          {lastThree?.map(s =>{
+            return(
+              <SermonCard
+                key={s.id}
+                title={s.title}
+                date={s.date}
+                verse={s.verse}
+                description={s.description}
+                cover={s.cover}
+              />
+            )
+          })}
+        </div>
+        {
+          lastFour.length > 0 ? (
+        <div className='mt-6'>
+          <div className='flex justify-between items-center mb-4'>
+            <h3 className='text-xl font-semibold '>Contactos</h3>
+            <NavLink><button className=' rounded-lg bg-[#be8552] border-2 border-[#be8552] px-4 my-4'>Ver más contactos</button></NavLink>
+          </div>
+          <div>
+          {lastFour?.map(s =>{
+            return(
+              <ContactCard
+                key={s.id}
+                name={s.name}
+                lastName={s.lastName}
+                phone={s.phone}
+                email={s.email}
+                message={s.message}
+              />
+            )
+          })}
+          </div>
+        </div>
+          ) : null
+        }
     </div>
   )
 }
